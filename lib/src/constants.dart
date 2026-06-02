9d154247f902124d06ed603212ae364c601cad8f
sebbom <sebastien.bomart@gmail.com>
2026-06-02 18:42:08 +0200

    constant definition

---
+++ lib/src/constants.dart
@@ -0,0 +1,9 @@
+class SttConstants {
+  SttConstants._();
+
+  static const int targetSampleRate = 16000;
+  static const int hopLength = 160;
+  static const int maxFrames = 3000;
+
+  static const int defaultTimeoutMs = 120000;
+}
